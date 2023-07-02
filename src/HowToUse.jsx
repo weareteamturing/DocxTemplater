d52f72cd126c58b34df52a020dae0ea028640f5d
최병욱 <esc5221@gmail.com>
2023-07-02 20:19:49 +0900

    [fix] image loading

--- src/HowToUse.jsx
+++ src/HowToUse.jsx
@@ -11,7 +11,7 @@ function HowToUse() {
                 title="1. Template.docx 파일과 csv 파일을 예시와 같이 작성"
                 subComponent={
                     <div>
-                        <img src="src/assets/template_example.png" />
+                        <img src="template_example.png" />
                     </div>
                 }
             />
@@ -20,7 +20,7 @@ function HowToUse() {
                 title="3. output file format을 원하는 포맷으로 작성"
                 subComponent={
                     <div>
-                        <img src="src/assets/output_format_example.png" />
+                        <img src="output_format_example.png" />
                         <li style={{ textAlign: "left" }}>
                             기본적으로 csv의 열 순서를 나타내는 <b>{`{rowNum}`}</b>을 사용할 수 있습니다.
                         </li>
